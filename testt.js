d82b3cd9faf213fc3a421f203b322788235f6ed9
RamCreator <rammaye87540644@gmail.com>
2024-03-10 14:09:38 +0530

    new_game

---
+++ testt.js
@@ -0,0 +1,31 @@
+console.log( "randon " + Math.trunc(Math.random() * 7))
+console.log( "rand " + (Math.random() * 7))
+
+name = "shiva"
+
+b = name === "shiva"  ? "shiva" : "ram"
+
+console.log("b = " + b)
+
+scores = [0,0]
+currentScore = 10 
+activeplayer = 0
+
+//scores[activeplayer] >= 100
+
+
+scores[activeplayer] += currentScore;
+scores[activeplayer] = scores[activeplayer] + currentScore
+scores[0] = scores[0] + 10
+scores[0] = 0 + 10 
+
+
+
+
+
+// scores[10,0]
+
+
+
+
+
